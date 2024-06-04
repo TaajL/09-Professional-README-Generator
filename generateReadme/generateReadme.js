1428df90ce5cb5d5bb449c763d4439f154791390
Malcom X <Taajlogan@gmail.com>
2024-06-04 16:37:50 -0400

    tableofcontents

--- generateReadme/generateReadme.js
+++ generateReadme/generateReadme.js
@@ -5,13 +5,13 @@ const generateReadme = (answers) => {
   ![License](https://img.shields.io/badge/License-${license}-blue.svg)
 
   ## Table of Contents
-  * [Description](https://github.com/TaajL/09-Professional-README-Generator/tree/main/generatedREADMEs#description)
-  * [Installation Instructions](https://github.com/TaajL/09-Professional-README-Generator/tree/main/generatedREADMEs#installation-instructions)
-  * [Usage](https://github.com/TaajL/09-Professional-README-Generator/tree/main/generatedREADMEs#usage)
-  * [License](https://github.com/TaajL/09-Professional-README-Generator/tree/main/generatedREADMEs#license)
-  * [Credits](https://github.com/TaajL/09-Professional-README-Generator/tree/main/generatedREADMEs#credits)
-  * [Test Instructions](https://github.com/TaajL/09-Professional-README-Generator/tree/main/generatedREADMEs#test-instructions)
-  * [Questions?](https://github.com/TaajL/09-Professional-README-Generator/tree/main/generatedREADMEs#questions)
+  * [Description](https://github.com/TaajL/09-Professional-README-Generator/blob/main/OUTPUT.md#description)
+  * [Installation Instructions](https://github.com/TaajL/09-Professional-README-Generator/blob/main/OUTPUT.md#installation)
+  * [Usage](https://github.com/TaajL/09-Professional-README-Generator/blob/main/OUTPUT.md/#usage)
+  * [License](https://github.com/TaajL/09-Professional-README-Generator/blob/main/OUTPUT.md/#license)
+  * [Credits](https://github.com/TaajL/09-Professional-README-Generator/blob/main/OUTPUT.md/#credits)
+  * [Test Instructions](https://github.com/TaajL/09-Professional-README-Generator/blob/main/OUTPUT.md/#Test)
+  * [Questions?](https://github.com/TaajL/09-Professional-README-Generator/blob/main/OUTPUT.md/#contact)
 
   ## Description
     ${description}
